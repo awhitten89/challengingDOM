e3caa7709329fb3ace7d279f93a58d250dfa2c7f
alanwhitten <awhitten89@gmail.com>
2020-07-26 21:37:08 +0100

    added Tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('getTableRow', () => {
+    cy.get('tbody > tr')
+})
+
+Cypress.Commands.add('getCell', (row, cell) => {
+    cy.get('tbody > tr').eq(row).find('td').eq(cell)
+})
